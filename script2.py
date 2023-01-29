8d81915d0ce9c113f7307d387d078943c88af367
memoha <Megha.Mohan@flemingcollege.ca>
2023-01-28 22:44:09 -0500

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added two point() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Beautiful is better than ugly.")
 print("Explicit is better than implicit.")
+print("Namespaces are one honking great idea -- let's do more of those!")
